266c7856beb668c329ea0568b8b13b0f6a62a54c
Ron Andrews <ron.andrews@gmail.com>
2016-08-26 03:20:32 -0400

    prepare for heroku

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   has_many :boards, dependent: :destroy
-  belongs_to :final, class_name: 'Clue', :foreign_key => 'final_id'
+  belongs_to :final, class_name: 'Clue', foreign_key: 'final_id'
   accepts_nested_attributes_for :boards
   accepts_nested_attributes_for :final
 end
